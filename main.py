6b87f52cc18a9c40e19fb5cb35c494180cd536d4
maitrimshah <maitri-mayank.shah@capgemini.com>
2024-03-19 12:45:20 +0000

    ui and vertex ai call changes

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
-from flask import Flask, render_template
-
+from datetime import datetime
+import os      
 import llm_generate
-app = Flask(__name__)
+from flask import Flask, flash, request, redirect, render_template
 
+app = Flask(__name__)
+os.urandom(12)
+
+# Allowed extension 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/upload_files", methods=["POST"])
+def upload_files():
+    if request.method == 'POST':
+        if 'files[]' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        files = request.files.getlist('files[]')
+
+        for file in files:
+          if not allowed_file(file.filename):
+            return "Received image file is not supported"
+        
+      
+        #for file in files:
+          # TO DO - to remove hardcoding related to images,create image array and pass it as arg in line 36 call
+            #flash('File ' + file.name + ' uploaded successfully')
+
+        #flash('All images successfully uploaded')
+        
+        llm_response = llm_generate.generateContent()
+  
+        if llm_response[0]:
+            print('car_details')
+            print(llm_response[0])
+        if llm_response:
+            print('spare_parts')
+            print(llm_response)
+            return render_template('models.html',car_details=llm_response[0],spare_parts=llm_response)
+        return "Sever response is not available."
+        
+    
 @app.route("/")
 def index():
-
-    llm_response = llm_generate.generate_content()
-    if llm_response:
-      print(llm_response)
-      return render_template('home.html', car_models=llm_response)
-    return "Sever response is not available."
-
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
+    
+    
